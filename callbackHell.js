0c78ea474a2fd401c5b6ee6dcc82242b96d325e4
Garrett Bodley <garrett.bodley@gmail.com>
2023-09-26 00:50:09 -0400

    I made a callback hell

--- callbackHell.js
+++ callbackHell.js
@@ -1,33 +1,55 @@
-const fs = require("node:fs")
-const path = require("node:path")
-const readline = require("node:readline")
+const fs = require("node:fs");
+const path = require("node:path");
+const readline = require("node:readline");
 
-FILEPATHS = [
+TEXT_PATHS = [
   "textfiles/in-search-of-lost-time.txt",
   "textfiles/moby-dick.txt",
-  "textfiles/ulysses.txt"
-]
+  "textfiles/ulysses.txt",
+];
 
-function callbackHell(){
-  const [inPath, outPath] = parsePath(FILEPATHS[0])
-  fs.readFile(inPath, {encoding: 'utf-8'}, (err, data) => {
-    if(err) throw err
-    data.split(/\n/).forEach(line => {
-      console.log(`Line to append:\n${line}`)
-      fs.appendFile(outPath, `${line}\n`, {encoding: 'utf-8'}, (err) => {
-        if(err) throw err
-        console.log(`New line appended to ${outPath}`)
-      }) 
-    })
-  })
+OUTPUT_PATH = "output/output.txt";
 
+function callbackHell() {
+  const proustPath = getAbsolutePath(TEXT_PATHS[0]);
+  const melvillePath = getAbsolutePath(TEXT_PATHS[1]);
+  const joycePath = getAbsolutePath(TEXT_PATHS[2]);
+  const absOutputPath = getAbsolutePath(OUTPUT_PATH);
+  debugger;
+
+  let proustData;
+  let melvilleData;
+  let joyceData;
+  let megaDataLength = 0;
+
+  fs.readFile(proustPath, { encoding: "utf-8" }, (err, data) => {
+    if (err) throw Error(err.message);
+    proustData = data;
+    fs.readFile(melvillePath, { encoding: "utf-8" }, (err, data) => {
+      if (err) throw Error(err.message);
+      melvilleData = data;
+      fs.readFile(joycePath, { encoding: "utf-8" }, (err, data) => {
+        if (err) throw Error(err.message);
+        joyceData = data;
+        let megaData = [proustData, melvilleData, joyceData].join("\n");
+        megaDataLength = megaData.length;
+        debugger;
+        // megaData.split(/\n/).length = 157918
+        fs.writeFile(absOutputPath, megaData, (err) => {
+          if (err) throw Error(err.message);
+          fs.readFile(absOutputPath, { encoding: "utf-8" }, (err, data) => {
+            if (err) throw Error(err.message);
+            if ((data.length = megaDataLength)) console.log("Lengths Match!");
+            else console.log("Lengths do not match!");
+          });
+        });
+      });
+    });
+  });
 }
 
-function parsePath(relativePath){
-  const parseObj = path.parse(path.join(__dirname, relativePath))
-  const inPath = path.join(parseObj.dir, parseObj.base)
-  const outPath = path.join(__dirname, "copied", parseObj.base)
-  return [inPath, outPath]
+function getAbsolutePath(relativePath) {
+  return path.join(__dirname, relativePath);
 }
 
-callbackHell()
+callbackHell();
