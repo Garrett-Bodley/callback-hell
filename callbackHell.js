2580abe77b09d846a5982de6aaa58bd3dd72cc95
Garrett-Bodley <garrett.bodley@gmail.com>
2023-10-06 16:22:35 -0400

    Promise Purgatory Draft

--- callbackHell.js
+++ callbackHell.js
@@ -2,13 +2,13 @@ const fs = require("node:fs");
 const path = require("node:path");
 const readline = require("node:readline");
 
-TEXT_PATHS = [
+const TEXT_PATHS = [
   "textfiles/in-search-of-lost-time.txt",
   "textfiles/moby-dick.txt",
   "textfiles/ulysses.txt",
 ];
 
-OUTPUT_PATH = "output/output.txt";
+const OUTPUT_PATH = "output/output.txt";
 
 function callbackHell() {
 
@@ -17,33 +17,85 @@ function callbackHell() {
   let proustData;
   let melvilleData;
   let joyceData;
-  let megaDataLength = 0;
 
   fs.readFile(proustPath, { encoding: "utf-8" }, (err, data) => {
     if (err) throw Error(err.message);
     proustData = data;
+    console.log('there')
     fs.readFile(melvillePath, { encoding: "utf-8" }, (err, data) => {
       if (err) throw Error(err.message);
       melvilleData = data;
       fs.readFile(joycePath, { encoding: "utf-8" }, (err, data) => {
         if (err) throw Error(err.message);
         joyceData = data;
-        let megaData = [proustData, melvilleData, joyceData].join("\n");
-        megaDataLength = megaData.length;
-        debugger;
-        // megaData.split(/\n/).length = 157918
-        fs.writeFile(absOutputPath, megaData, (err) => {
-          if (err) throw Error(err.message);
-          fs.readFile(absOutputPath, { encoding: "utf-8" }, (err, data) => {
-            if (err) throw Error(err.message);
-            if ((data.length = megaDataLength)) console.log("Lengths Match!");
-            else console.log("Lengths do not match!");
-          });
-        });
+        let megaData = [proustData, melvilleData, joyceData].join("");
+        fs.writeFile(absOutputPath, '', (err) => {
+          if(err) throw Error(err.message)
+          let count = 0
+          function checkFile(count){
+            if(count < 3) return
+            fs.readFile(absOutputPath, { encoding: "utf-8" }, (err, data) => {
+              if (err) throw Error(err.message);
+              if (data === megaData) console.log("Test Passed!");
+              else console.log("Lengths do not match!");
+            });
+          }
+          fs.appendFile(absOutputPath, proustData, () => checkFile(++count))
+          fs.appendFile(absOutputPath, melvilleData, () => checkFile(++count))
+          fs.appendFile(absOutputPath, joyceData, () => checkFile(++count))
+        })
       });
     });
   });
+
+  console.log('here')
 }
+
+function promisePurgatory(){
+  const [proustPath, melvillePath, joycePath, absOutputPath] = bundlePaths(TEXT_PATHS, OUTPUT_PATH)
+  let proustData;
+  let melvilleData;
+  let joyceData;
+  let megaData;
+
+  return fs.promises.readFile(proustPath, {encoding: 'utf-8'})
+    .then(data => {
+      proustData = data
+      // return Promise.resolve()
+    }).then(() => {
+      return fs.promises.readFile(melvillePath, {encoding: 'utf-8'})
+    }).then(data => {
+      melvilleData = data
+    }).then(() => {
+      return fs.promises.readFile(joycePath, {encoding: 'utf-8'})
+    }).then(data => {
+      joyceData = data
+    }).then(() => {
+      megaData = proustData + melvilleData + joyceData
+    }).then(() => {
+      return fs.promises.writeFile(absOutputPath, '')
+    }).then(() => {
+      // this does not write in order
+      // return Promise.all([
+      //   fs.promises.appendFile(absOutputPath, proustData),
+      //   fs.promises.appendFile(absOutputPath, melvilleData),
+      //   fs.promises.appendFile(absOutputPath, joyceData)
+      // ])
+
+      // You must chain fs.promises.appendFile for sequential writes
+      return fs.promises.appendFile(absOutputPath, proustData)
+      .then(() => fs.promises.appendFile(absOutputPath, melvilleData))
+      .then(() => fs.promises.appendFile(absOutputPath, joyceData))
+    }).then(() => {
+      return fs.promises.readFile(absOutputPath, {encoding: 'utf-8'})
+    }).then((data) => {
+      if(data === megaData) console.log('Files Match!')
+      else console.log("Lengths do not match!")
+    }).catch(err => {
+      console.log(err.message)
+    })
+}
+
 
 function bundlePaths(textPaths, outputPath){
   const proustPath = getAbsolutePath(textPaths[0]);
@@ -58,4 +110,5 @@ function getAbsolutePath(relativePath) {
   return path.join(__dirname, relativePath);
 }
 
-callbackHell();
+// callbackHell();
+promisePurgatory()
